e166345c3696b58fb80a87f2359b926c4118c4d9
Wesley Merkel <ooesili@gmail.com>
2023-10-27 21:24:23 -0600

    Conform to flake output schema

--- flake.nix
+++ flake.nix
@@ -17,34 +17,34 @@
     flake-utils,
     rust-overlay,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      rust-xtensa-overlay = final: prev: {
-        llvmPackages_xtensa = prev.llvmPackages_16.override {
-          monorepoSrc = let
-            espressif-llvm = prev.fetchFromGitHub {
-              owner = "espressif";
-              repo = "llvm-project";
-              rev = "0b255ec2fb67b3469af4b7e1336a64ed60f4e251";
-              sha256 = "sha256-bnSnyPq30Cc6bz6T/FVMgdBk6HGmgTTgRirJMBWDC0c=";
-            };
-          in
-            final.runCommand "espressif-llvm-prepatched" {} ''
-              cp -r ${espressif-llvm} $out
-              chmod -R u+w $out
-              cd $out/llvm
-              patch --strip=1 < ${./nix-llvm-prepatch.patch}
-            '';
-        };
-
-        rust-esp = prev.callPackage ./esp-rs-rust.nix {
-          inherit (pkgs.darwin.apple_sdk.frameworks) CoreFoundation Security SystemConfiguration;
-          llvm_16 = final.llvmPackages_xtensa.libllvm;
-          llvmPackages_16 = final.llvmPackages_xtensa;
-          rustNixPath = "${nixpkgs}/pkgs/development/compilers/rust/default.nix";
-        };
+  }: let
+    rust-xtensa-overlay = final: prev: {
+      llvmPackages_xtensa = prev.llvmPackages_16.override {
+        monorepoSrc = let
+          espressif-llvm = prev.fetchFromGitHub {
+            owner = "espressif";
+            repo = "llvm-project";
+            rev = "0b255ec2fb67b3469af4b7e1336a64ed60f4e251";
+            sha256 = "sha256-bnSnyPq30Cc6bz6T/FVMgdBk6HGmgTTgRirJMBWDC0c=";
+          };
+        in
+          final.runCommand "espressif-llvm-prepatched" {} ''
+            cp -r ${espressif-llvm} $out
+            chmod -R u+w $out
+            cd $out/llvm
+            patch --strip=1 < ${./nix-llvm-prepatch.patch}
+          '';
       };
 
+      rust-esp = prev.callPackage ./esp-rs-rust.nix {
+        inherit (final.darwin.apple_sdk.frameworks) CoreFoundation Security SystemConfiguration;
+        llvm_16 = final.llvmPackages_xtensa.libllvm;
+        llvmPackages_16 = final.llvmPackages_xtensa;
+        rustNixPath = "${nixpkgs}/pkgs/development/compilers/rust/default.nix";
+      };
+    };
+  in
+    (flake-utils.lib.eachSystem flake-utils.lib.allSystems (system: let
       pkgs = import nixpkgs {
         inherit system;
         overlays = [
@@ -53,8 +53,6 @@
         ];
       };
     in {
-      overlays.default = rust-xtensa-overlay;
-
       devShells.default = pkgs.mkShell {
         buildInputs = [
           pkgs.arduino
@@ -69,5 +67,8 @@
           pkgs.rust-esp.packages.stable.rustc
         ];
       };
-    });
+    }))
+    // {
+      overlays.default = rust-xtensa-overlay;
+    };
 }
